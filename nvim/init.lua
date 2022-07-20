1dd8cd3e74253147901528f09afbe9af25e37461
Hussain Shafeeu <shxfee@gmail.com>
2022-07-21 01:12:45 +0500

    feat: add neorg telescope keybindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -301,6 +301,25 @@ require('neorg').setup {
     },
 }
 
+-- Bind neorg telescope keys
+local neorg_callbacks = require("neorg.callbacks")
+
+neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
+    -- Map all the below keybinds only when the "norg" mode is active
+    keybinds.map_event_to_mode("norg", {
+        n = { -- Bind keys in normal mode
+            { "<SPACE>ns", "core.integrations.telescope.find_linkable" },
+        },
+
+        i = { -- Bind in insert mode
+            { "<C-l>", "core.integrations.telescope.insert_link" },
+        },
+    }, {
+        silent = true,
+        noremap = true,
+    })
+end)
+
 -- personal plugs
 dofile(config_path .. '/lua/my/statusline.lua')         -- status & tabline
 dofile(config_path .. '/lua/my/playground.lua')         -- temporary stuff
