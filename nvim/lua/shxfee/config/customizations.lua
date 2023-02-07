3fdedd4131b148414c177f6d23b7005a46b8d8ac
Hussain Shafeeu <shxfee@gmail.com>
2023-02-07 22:41:29 +0500

    add diagnostic signs

--- nvim/lua/shxfee/config/customizations.lua
+++ nvim/lua/shxfee/config/customizations.lua
@@ -5,3 +5,16 @@ nnoremap <expr> k (v:count > 1 ? "m'" . v:count : '') . 'k'
 nnoremap <expr> j (v:count > 1 ? "m'" . v:count : '') . 'j'
 vnoremap // y/\V<C-R>=escape(@",'/\')<CR><CR>
 ]], false)
+
+-- lsp diagnostics signs
+local signs = {
+  Error = "",
+  Warn = "",
+  Hint = "",
+  Info = ""
+}
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
